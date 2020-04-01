de206ee450060a882cd7190957726a0da6a3aba7
juntae <gkwnsxo1234@gmail.com>
2020-04-01 18:50:15 +0900

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "테스트",
+        "excerpt":"오늘  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/",
+        "teaser": null
+      }]

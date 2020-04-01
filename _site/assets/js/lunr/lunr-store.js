3e32f7ddf75cb69e133657360b75e3e5def8f721
juntae <gkwnsxo1234@gmail.com>
2020-04-01 19:02:54 +0900

    save

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "테스트",
-        "excerpt":"오늘  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/",
-        "teaser": null
-      }]
+var store = []

b6cd1aa98177bf03b58f38be50e57f8c5dafcbff
juntae <gkwnsxo1234@gmail.com>
2020-04-02 21:05:58 +0900

    파일생성, 리다이렉션, 파일삭제 수정 최종

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "[nodejs] 파일생성, 리다이렉션, 파일삭제",
-        "excerpt":"파일생성과 리다이렉션  response.writeHead(302, {Location : 'http://whateverhostthiswillbe:8675/' +newRoom}); response.end();   writeHead(200) 은 성공시켰다. writeHead(302) 페이지를 다른데로 리다이렉션 시킨다.   ex)  fs.writeFile(`data/${title}`, description, 'utf8', function(err){             response.writeHead(302, {Location: `/?id=${title}`});             response.end();           })   fs.unlink(path, callback) =&gt; 파일삭제 ex)  fs.unlink(`data/${id}`, function(error){             response.writeHead(302, {Location: `/`});             response.end();           })  삭제가 되고 홈으로 이동한다.   Get 방식 QueryString 이 있는 것  ","categories": [],
+        "excerpt":"파일생성과 리다이렉션 리다이렉션 : 출력의 방향을 바꾼다. response.writeHead(302, {Location : 'http://whateverhostthiswillbe:8675/' +newRoom}); response.end(); writeHead(200) 은 성공시켰다. writeHead(302) 페이지를 다른데로 리다이렉션 시킨다. ex) fs.writeFile(`data/${title}`, description, 'utf8', function(err){ response.writeHead(302, {Location: `/?id=${title}`}); response.end(); }) fs.unlink(path, callback) =&gt; 파일삭제 ex) fs.unlink(`data/${id}`, function(error){ response.writeHead(302, {Location: `/`}); response.end(); }) 삭제가 되고 홈으로 이동한다. Get 방식 QueryString...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/nodejs/",
         "teaser": null

14b3298f9673cabefb4e5e62388b1b92e8b1156d
juntae <gkwnsxo1234@gmail.com>
2020-04-16 10:33:06 +0900

    Mysql Module

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -58,4 +58,10 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/aws/",
         "teaser": null
+      },{
+        "title": "[nodejs]Mysql Module",
+        "excerpt":"기본적으로 nodejs 모듈에는 mysql를 제어하는 모듈이 없다. pm install -S mysql 을 해주면 packson(dependencies) 에 추가가된다. 소스코드를 나중에 복제하고 사용할때 편리하기 위해 db.query(SELECT * FROM topic WHERE id=${query.id}`,function(error2,topic){ //이런식으로 쓰면 사용자에게 공격을 당할 수 있다. db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id],function(error2,topic){ //id에 ? 에 두번쨰 인자가 치환되서 들어간다.보안성이좋음 response.writeHead(302, {Location:...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/nodejsmysql/",
+        "teaser": null
       }]

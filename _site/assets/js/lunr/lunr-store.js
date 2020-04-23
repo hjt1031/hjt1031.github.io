9d735a553b424c59f90105a0b2fc6076e025b58b
juntae <gkwnsxo1234@gmail.com>
2020-04-23 13:18:53 +0900

    jun

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -89,7 +89,7 @@ var store = [{
         "url": "http://localhost:4000/cookie-session-cache/",
         "teaser": null
       },{
-        "title": "[d]",
+        "title": "vkdjfl",
         "excerpt":"방식, 방법론이다. ( 객체지향 언어는 복잡한 코드를 간결하게? 해주는 것이라 생각하면 됨.)   ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/",

188c69ed683bbbfd604988e3b0c5a6dc05a3c547
hjt1031 <hjt1031@naver.com>
2020-08-20 22:41:26 +0900

    router

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -352,4 +352,10 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/react-nomadmap/",
         "teaser": null
+      },{
+        "title": "[React Nomad]react-router-dom",
+        "excerpt":"import {HashRouter, Route} from \"react-router-dom\"; //react-router-dom은 다른 종류의 라우터가 있다 그중에 하나가 HashRouter,Route function App(){ //함수형 컨포넌트 생성 return &lt;HashRouter&gt; &lt;Route path=\"/\" component={Home}/&gt; &lt;Route path=\"/about\" component={About}/&gt; &lt;/HashRouter&gt; } 2개의 컨포넌트가 같이 렌더링이 된다. why? 리액트는 쉽게 말해 자신이 찾는 문자가 속해 했으면 그것도 렌더링을 해버리기 때문? 그래서 path=”/” 을 먼저...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/react-nomadreact-router-dom/",
+        "teaser": null
       }]

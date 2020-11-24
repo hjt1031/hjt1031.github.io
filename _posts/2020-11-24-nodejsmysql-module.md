470c28e9244a2fa4c0f770048e787da9c5b1f926
hjt1031 <hjt1031@naver.com>
2020-11-24 22:36:45 +0900

    블로그 오류 수정 java chap03

---
+++ _posts/2020-11-24-nodejsmysql-module.md
@@ -0,0 +1,14 @@
+---
+title: "[nodejs]Mysql Module"
+---
+
+기본적으로 nodejs 모듈에는 mysql를 제어하는 모듈이 없다. 
+pm install -S mysql 을 해주면 packson(dependencies) 에 추가가된다.  소스코드를 나중에 복제하고 사용할때 편리하기 위해
+
+
+```java
+db.query(SELECT * FROM topic WHERE id=${query.id}`,function(error2,topic){ //이런식으로 쓰면 사용자에게 공격을 당할 수 있다.
+
+db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id],function(error2,topic){ //id에 ? 에 두번쨰 인자가 치환되서 들어간다.보안성이좋음
+
+ response.writeHead(302, {Location: `/?id=${result.insertId}`}); //추가된 id 을 가져오는 방법 (바로 추가된 id로 리다이렉션이 됨)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,20 +1,8 @@
 var store = [{
-        "title": "[nodejs] 파일생성, 리다이렉션, 파일삭제",
-        "excerpt":"파일생성과 리다이렉션 리다이렉션 : 출력의 방향을 바꾼다. response.writeHead(302, {Location : 'http://whateverhostthiswillbe:8675/' +newRoom}); response.end(); writeHead(200) 은 성공시켰다. writeHead(302) 페이지를 다른데로 리다이렉션 시킨다. ex) fs.writeFile(`data/${title}`, description, 'utf8', function(err){ response.writeHead(302, {Location: `/?id=${title}`}); response.end(); }) fs.unlink(path, callback) =&gt; 파일삭제 ex) fs.unlink(`data/${id}`, function(error){ response.writeHead(302, {Location: `/`}); response.end(); }) 삭제가 되고 홈으로 이동한다. Get 방식 QueryString...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/nodejs/",
-        "teaser": null
-      },{
         "title": "[AWS]기초용어",
         "excerpt":"EC2 : Elastic Compute Cloud, 아마존 웹 서비스 중 대표적인 상품 Instance : Instance 한개당 한대의 컴퓨터라 생각하면 된다 온디맨드 인스턴스 : 필요할때 키고 필요없을땐 끊다. 우리가 컴퓨터를 필요할 때마다 켜고 끄고 할 수 있는 인스턴스를 의미 예약 인스턴스 : 서버를 키는데 365일 동안 끄지 않는 경우면 예약 인스턴스를 사용하는게...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/aws/",
-        "teaser": null
-      },{
-        "title": "[node js] 객체",
-        "excerpt":"Object 와 Array var members = ['egoging','k8805','hoya']; console.log(members[1]); var i = 0; while (i&lt;members.length) { console.log('array loop',members[i]); i = i+1; } var roles = { 'programmer':'egoing', // key : value 'designer' : 'k8805', 'manager' : 'hoya'} console.log(roles.designer) //객체는 . 을 찍어서 가져온다. console.log(roles['debugger']) //키값을 문자로 전달해도 가져올 수 있다. for(var...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/node-js/",
         "teaser": null
       },{
         "title": "[AWS] OSX로 AWS연결",
@@ -23,46 +11,10 @@ var store = [{
         "url": "http://localhost:4000/aws-osx-aws/",
         "teaser": null
       },{
-        "title": "[nodejs] 객체화, 모듈사용법,  경로보안, 출력정보 보안",
-        "excerpt":"코드를 간결하게 객체화 해서 사용가능 다른말로 리팩토링 이라고도 함 var template = { //template 라는 객체를 생성 HTML:function(title, list, body, control){ return ` &lt;!doctype html&gt; &lt;html&gt; &lt;head&gt; &lt;title&gt;WEB1 - ${title}&lt;/title&gt; &lt;meta charset=\"utf-8\"&gt; &lt;/head&gt; &lt;body&gt; &lt;h1&gt;&lt;a href=\"/\"&gt;WEB&lt;/a&gt;&lt;/h1&gt; ${list} ${control} ${body} &lt;/body&gt; &lt;/html&gt; `; },list:function(filelist){ var list = '&lt;ul&gt;'; var i =...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/nodejs/",
-        "teaser": null
-      },{
-        "title": "[AWS]인스턴스 에서 웹서버 사용",
-        "excerpt":"   홈페이지로 로그인해 연결을 눌러 키를 터미널에 복사 붙여넣기 를 해서 접속한다.   아마존 EC2 인스턴스에 퍼블릭 DNS(IPv4)에 있는 주소를 복사해 주소창에 붙여넣기를 한다.   홈페이지에 우분투 라고 나오면 제대로 접속한게 맞는것이다.  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/aws/",
-        "teaser": null
-      },{
         "title": "[AWS]AMI & WordPress 설치",
         "excerpt":"Amazon machine image : 컴퓨터가 갖고 있는 상태를 그대로 얼려서 나중에 똑같이 복원하는 데이터 (그컴퓨터에 운영체제 ,소프트웨어 설정 ,현재 사용중인 프로그램 등등) 백업 기능이라 생각하면 된다 Wordpress : 가상머신의 형태로 제공된 솔루션을 사고 팔 수 있는 공간이라고 보면 된다.(다른사람이 만든 인스턴스를 사용하는 것) wordpress 설치방법 aws markplace 에서 wordpresss 를...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/awsami/",
-        "teaser": null
-      },{
-        "title": "[AWS]Scalability",
-        "excerpt":"변화하는 수요에 얼마나 탄력적으로 공급을 할 수 있는가? 클라우드 컴퓨팅   Scale Up : 웹사이트를 운영하는데 접속자가 계속 많아지면 그 수요에 대해 어떻게 탄력적으로 어떻게 대응할 건지. (수요가 많으면 좋은 컴퓨터로 업그레이드)  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/awsscalability/",
-        "teaser": null
-      },{
-        "title": "[nodejs]API & Mysql",
-        "excerpt":"API : Application Programming interface   개발자와 함수를 사용하는 개발자 들의 약속된 조작장치  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/nodejsapi/",
-        "teaser": null
-      },{
-        "title": "[AWS]스트래스 테스트(ab test)",
-        "excerpt":"인스턴스 접속 : ssh -i 키위치/키이름.pem ubuntu@ip 컴퓨터의 CPU 점유율 보기 : top ssh -i Desktop/dev/key/aws_password.pem ubuntu@54.180.2.171 sudo apt-get update : 컴퓨터에 프로그램 목록을 최신버전으로 업그레이드 ab 프로그램 : 아파치에서 만든 부화발생기 (인스턴스 접속 후 ab 타이핑) requests : 어떤 웹서버에 몇번의 접속을 시도하겠냐 concurrency : 동시에 몇번의 접속을 시도하겠냐...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/aws/",
-        "teaser": null
-      },{
-        "title": "[nodejs]Mysql Module",
-        "excerpt":"기본적으로 nodejs 모듈에는 mysql를 제어하는 모듈이 없다. pm install -S mysql 을 해주면 packson(dependencies) 에 추가가된다. 소스코드를 나중에 복제하고 사용할때 편리하기 위해 db.query(SELECT * FROM topic WHERE id=${query.id}`,function(error2,topic){ //이런식으로 쓰면 사용자에게 공격을 당할 수 있다. db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id],function(error2,topic){ //id에 ? 에 두번쨰 인자가 치환되서 들어간다.보안성이좋음 response.writeHead(302, {Location:...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/nodejsmysql/",
         "teaser": null
       },{
         "title": "[AWS] Elastic Ips & Scale Up",
@@ -105,12 +57,6 @@ var store = [{
         "excerpt":"로드밸런서 생성을 하는데,   만약 우리의 서비스가 HTTPS 도 지원한다면 리스너 추가를 해서 HTTPS 를 추가하는데 인스턴스는 HTTPS 로 할필요없이 HTTP 로만 해줘도된다.   왜냐하면 로브밸런서와 EC2 사이에 로드밸런서가 안전하게 통신할 수 있기 때문에   DNS name 이 만들어지는데 DNS name 으로 접속이 가능하다.  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/aws-elbelastic-load-balancing/",
-        "teaser": null
-      },{
-        "title": "[AWS]터미널로 서버접속, ELB 적용(ab 테스터)",
-        "excerpt":"두개의 인스턴스를 생성해준다. $ ssh -i 터미널경로/aws_passward.pem ubuntu@public ip주소 $ sudo apt-get update 타이핑(ab 벤치마킹 테스터기, 업데이트 먼저 해줘야함)패키지 리스트 를 업데이트를 해준다. $ sudo apt-get install apache2-utils 타이핑(ab벤치마킹 설치), 웹서버 설치는 $ sudo apt-get install apache2 ELB적용 웹서버를 이미지를 만들어준다.(AMI) load balances 를 들어가 웹서버 2개를 save 해준다. DNS...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/aws/",
         "teaser": null
       },{
         "title": "[nodejs] Escaping (탈출)",
@@ -543,5 +489,71 @@ var store = [{
         "excerpt":"2행 4열의 2차원 배열 //2행 4열의 2차원 배열 package chap2; public class Int2DArray { public static void main(String[] args) { int[][] x = new int[2][4]; //2차원 배열 선언 x[0][1] = 37; x[0][3] = 54; x[1][2] = 65; for(int i = 0; i &lt; 2; i++) for(int j = 0; j...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/java/",
+        "teaser": null
+      },{
+        "title": "[aws]스트래스테스트",
+        "excerpt":"인스턴스 접속 : ssh -i 키위치/키이름.pem ubuntu@ip 컴퓨터의 CPU 점유율 보기 : top ssh -i Desktop/dev/key/aws_password.pem ubuntu@54.180.2.171 sudo apt-get update : 컴퓨터에 프로그램 목록을 최신버전으로 업그레이드 ab 프로그램 : 아파치에서 만든 부화발생기 (인스턴스 접속 후 ab 타이핑) requests : 어떤 웹서버에 몇번의 접속을 시도하겠냐 concurrency : 동시에 몇번의 접속을 시도하겠냐...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/a/",
+        "teaser": null
+      },{
+        "title": "[aws]인스턴스 에서 웹서버 사용",
+        "excerpt":"   홈페이지로 로그인해 연결을 눌러 키를 터미널에 복사 붙여넣기 를 해서 접속한다.   아마존 EC2 인스턴스에 퍼블릭 DNS(IPv4)에 있는 주소를 복사해 주소창에 붙여넣기를 한다.   홈페이지에 우분투 라고 나오면 제대로 접속한게 맞는것이다.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/aws-1/",
+        "teaser": null
+      },{
+        "title": "[AWS]터미널로 서버접속, ELB적용(ab 테스터)",
+        "excerpt":"두개의 인스턴스를 생성해준다. $ ssh -i 터미널경로/aws_passward.pem ubuntu@public ip주소 $ sudo apt-get update 타이핑(ab 벤치마킹 테스터기, 업데이트 먼저 해줘야함)패키지 리스트 를 업데이트를 해준다. $ sudo apt-get install apache2-utils 타이핑(ab벤치마킹 설치), 웹서버 설치는 $ sudo apt-get install apache2 ELB적용 웹서버를 이미지를 만들어준다.(AMI) load balances 를 들어가 웹서버 2개를 save 해준다. DNS...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/aws-elbab/",
+        "teaser": null
+      },{
+        "title": "[aws]기초용어",
+        "excerpt":"EC2 : Elastic Compute Cloud, 아마존 웹 서비스 중 대표적인 상품 Instance : Instance 한개당 한대의 컴퓨터라 생각하면 된다 온디맨드 인스턴스 : 필요할때 키고 필요없을땐 끊다. 우리가 컴퓨터를 필요할 때마다 켜고 끄고 할 수 있는 인스턴스를 의미 예약 인스턴스 : 서버를 키는데 365일 동안 끄지 않는 경우면 예약 인스턴스를 사용하는게...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/aws/",
+        "teaser": null
+      },{
+        "title": "[AWS]Scalability",
+        "excerpt":"변화하는 수요에 얼마나 탄력적으로 공급을 할 수 있는가? 클라우드 컴퓨팅   Scale Up : 웹사이트를 운영하는데 접속자가 계속 많아지면 그 수요에 대해 어떻게 탄력적으로 어떻게 대응할 건지. (수요가 많으면 좋은 컴퓨터로 업그레이드)  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/awsscalability/",
+        "teaser": null
+      },{
+        "title": "[JAVA]알고리즘 chap03 검색알고리즘",
+        "excerpt":"선형 검색 package chap03; import java.util.*; public class SeqSearch { //요솟수가 n인 배열a에서 key와 같은 요소를 선형 검색합니다. static int seqSearch(int[] a, int n, int key) { int i = 0; while(true) { if(i == n) return -1; //검색실패(-1을 반환) if(a[i] == key) return i; //검색 성공(인덱스를 반환)7 i++; }...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/javachap/",
+        "teaser": null
+      },{
+        "title": "[nodejs] 객체화,모듈사용법,경로보안,출력정보 보안",
+        "excerpt":"코드를 간결하게 객체화 해서 사용가능 다른말로 리팩토링 이라고도 함 var template = { //template 라는 객체를 생성 HTML:function(title, list, body, control){ return ` &lt;!doctype html&gt; &lt;html&gt; &lt;head&gt; &lt;title&gt;WEB1 - ${title}&lt;/title&gt; &lt;meta charset=\"utf-8\"&gt; &lt;/head&gt; &lt;body&gt; &lt;h1&gt;&lt;a href=\"/\"&gt;WEB&lt;/a&gt;&lt;/h1&gt; ${list} ${control} ${body} &lt;/body&gt; &lt;/html&gt; `; },list:function(filelist){ var list = '&lt;ul&gt;'; var i =...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/node-js-1/",
+        "teaser": null
+      },{
+        "title": "[Nodejs]파일생성, 리다이렉션, 파일삭제",
+        "excerpt":"파일생성과 리다이렉션 리다이렉션 : 출력의 방향을 바꾼다. response.writeHead(302, {Location : 'http://whateverhostthiswillbe:8675/' +newRoom}); response.end(); writeHead(200) 은 성공시켰다. writeHead(302) 페이지를 다른데로 리다이렉션 시킨다. ex) fs.writeFile(`data/${title}`, description, 'utf8', function(err){ response.writeHead(302, {Location: `/?id=${title}`}); response.end(); }) fs.unlink(path, callback) =&gt; 파일삭제 ex) fs.unlink(`data/${id}`, function(error){ response.writeHead(302, {Location: `/`}); response.end(); }) 삭제가 되고 홈으로 이동한다. Get 방식 QueryString...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/nodejs-1/",
+        "teaser": null
+      },{
+        "title": "[Nodejs]객체",
+        "excerpt":"Object 와 Array var members = ['egoging','k8805','hoya']; console.log(members[1]); var i = 0; while (i&lt;members.length) { console.log('array loop',members[i]); i = i+1; } var roles = { 'programmer':'egoing', // key : value 'designer' : 'k8805', 'manager' : 'hoya'} console.log(roles.designer) //객체는 . 을 찍어서 가져온다. console.log(roles['debugger']) //키값을 문자로 전달해도 가져올 수 있다. for(var...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/nodejs/",
+        "teaser": null
+      },{
+        "title": "[nodejs]API & Mysql",
+        "excerpt":"API : Application Programming interface   개발자와 함수를 사용하는 개발자 들의 약속된 조작장치  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/nodejsapi-mysql/",
+        "teaser": null
+      },{
+        "title": "[nodejs]Mysql Module",
+        "excerpt":"기본적으로 nodejs 모듈에는 mysql를 제어하는 모듈이 없다. pm install -S mysql 을 해주면 packson(dependencies) 에 추가가된다. 소스코드를 나중에 복제하고 사용할때 편리하기 위해 ```java db.query(SELECT * FROM topic WHERE id=${query.id}`,function(error2,topic){ //이런식으로 쓰면 사용자에게 공격을 당할 수 있다. db.query(SELECT * FROM topic WHERE id=?,[queryData.id],function(error2,topic){ //id에 ? 에 두번쨰 인자가 치환되서 들어간다.보안성이좋음 response.writeHead(302,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/nodejsmysql-module/",
         "teaser": null
       }]
